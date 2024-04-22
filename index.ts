29f45338dd70edb1d1114b07f8ebbbb3045896fe
DUPLESSIS Alexis <98809630+meizarkos@users.noreply.github.com>
2024-04-22 21:53:03 +0200

    Prod ok

--- index.ts
+++ index.ts
@@ -26,6 +26,4 @@ app.use((_req, res) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("listening on port 3000!");
-});
+app.listen(3000,'127.0.0.1', () =>{ console.log(`Notre application Node est démarrée sur : https://helpother.fr`);});
