a6289e345e055810e6cb87145db022f79ae05139
DUPLESSIS Alexis <98809630+meizarkos@users.noreply.github.com>
2024-05-16 12:05:00 +0200

    Modif of

---
+++ src/routes/team/getName.ts
@@ -0,0 +1,20 @@
+import { Team } from '../../models';
+import { Application, Request, Response } from 'express';
+
+export const getIfNameIsOk = (app: Application) => {
+  app.get('/teamName/:teamName', async (req: Request, res: Response) => {
+    try {
+      const teamName = req.params.teamName;
+      const teams = await Team.findAll({where:{team_name:teamName}});
+      if(teams){
+        res.status(200).json({message:"Name is already taken"});
+      }
+      else{
+        res.status(200).json({message:"Name is available"});
+      }
+    } catch (e: unknown) {
+      console.log(e)
+      res.status(500).send({ error: "Internal server error" });
+    }
+  });
+}
