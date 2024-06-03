660854df3c92e0cb089f854003aa2fdfbb59d4e8
DUPLESSIS Alexis <98809630+meizarkos@users.noreply.github.com>
2024-06-03 18:52:49 +0200

    Modif of name

--- src/routes/employe/getAllEmploye.ts
+++ src/routes/employe/getAllEmploye.ts
@@ -8,12 +8,12 @@ export const get_all_employe_with_a_status = (app: Application) => {
       const employe = await Promise.all(employeTeams.map(async employe =>{
         const uuid = employe.getDataValue('uuid');
         const dataValue = employe.get();
-        const isInTheTeam = await EmployeTeam.findOne({where:{employeId:uuid,teamId:req.params.teamId}})
+        const isInTheTeam = await EmployeTeam.findOne({where:{employe_id:uuid,team_id:req.params.teamId}})
         if(isInTheTeam){
-          return {...dataValue, is_in_the_team:true}
+          return {...dataValue, is_in_the_team:"true"}
         }
         else{
-          return {...dataValue, is_in_the_team:false}
+          return {...dataValue, is_in_the_team:"false"}
         }
       }));
       res.status(200).json(employe);
