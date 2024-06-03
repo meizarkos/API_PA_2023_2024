037b978cf33bf60bd503f06c4fdf1c25677e3a87
DUPLESSIS Alexis <98809630+meizarkos@users.noreply.github.com>
2024-06-03 18:25:54 +0200

    Modif of name

--- src/routes/team/getEmploye.ts
+++ src/routes/team/getEmploye.ts
@@ -6,7 +6,7 @@ export const getAllEmployeeFromATeam = (app: Application) => {
     try {
       const employeTeams = await EmployeTeam.findAll({where:{team_id:req.params.teamId}})
       const employeIds = employeTeams.map(employeTeam => employeTeam.getDataValue('employe_id'));
-      const employes = await Employe.findAll({ where: { uuid: employeIds },attributes: { exclude: ['password'] } });
+      const employes = await Employe.findAll({ where: { uuid: employeIds },attributes: { exclude: ['password','rib'] } });
       res.status(200).json(employes);
     } catch (e: unknown) {
       console.log(e)
