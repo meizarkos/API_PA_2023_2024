20cc55de52941ec1633f8485ff5ad6c8d0116d1a
DUPLESSIS Alexis <98809630+meizarkos@users.noreply.github.com>
2024-06-05 19:43:15 +0200

    Add of build

---
+++ src/routes/response/postResponse.ts
@@ -0,0 +1,17 @@
+import { Response as ResponseModel} from '../../models';
+import { Application, Request, Response } from 'express';
+import { classByOlder } from '../../utils';
+
+export const postATicket= (app: Application) => {
+  app.get('/responseAdmin/:ticketId', async (req: Request, res: Response) => {
+    try {
+      req.body.ticket_id = req.params.ticketId;
+      req.body.creator_id = req.jwt.payload.id;
+      await ResponseModel.create(req.body);
+      res.status(200).json({ message: "Response created" });
+    } catch (e: unknown) {
+      console.log(e)
+      res.status(500).send({ error: "Internal server error" });
+    }
+  });
+}
