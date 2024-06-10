3ce2831fc4cc1fe597cb96c1f619ee2567fd06ec
DUPLESSIS Alexis <98809630+meizarkos@users.noreply.github.com>
2024-06-10 15:57:17 +0200

    Add of all admin data

--- src/routes/company/getAllDataAdmin.ts
+++ src/routes/company/getAllDataAdmin.ts
@@ -1,14 +1,14 @@
-import { Company,Ticket,Leave, Employe } from '../../models';
+import { Company,Ticket,Leave, Team } from '../../models';
 import { Application, Request, Response } from 'express';
 
 export const allAdminData = (app: Application) => {
   app.get('/allAdminData', async (req: Request, res: Response) => {
     try {
       const companies = await Company.findAll({where: {role: "ban"},attributes: { exclude: ["password"] }});
-      const employes = await Employe.findAll();
+      const teams = await Team.findAll();
       const tickets = await Ticket.findAll({where: {status: 0}});
       const leaves = await Leave.findAll({where: {status: "pending"}});
-      res.status(200).json({companyNumber: companies.length, employeNumber: employes.length, ticketNumber: tickets.length, leaveNumber: leaves.length});
+      res.status(200).json({companyNumber: companies.length, teamNumber: teams.length, ticketNumber: tickets.length, leaveNumber: leaves.length});
     } catch (e: unknown) {
       console.log(e)
       res.status(500).send({ error: "Internal server error" });
