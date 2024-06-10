251eb8d4753bd5aad1f030557a06d2f3e258cb59
DUPLESSIS Alexis <98809630+meizarkos@users.noreply.github.com>
2024-06-10 14:50:29 +0200

    Add of all admin data

---
+++ src/routes/company/getAllDataAdmin.ts
@@ -0,0 +1,17 @@
+import { Company,Team,Ticket,Leave } from '../../models';
+import { Application, Request, Response } from 'express';
+
+export const allAdminData = (app: Application) => {
+  app.get('/banCompanies', async (req: Request, res: Response) => {
+    try {
+      const companies = await Company.findAll({where: {role: "ban"},attributes: { exclude: ["password"] }});
+      const teams = await Team.findAll();
+      const tickets = await Ticket.findAll({where: {status: 0}});
+      const leaves = await Leave.findAll({where: {status: "pending"}});
+      res.status(200).json({companyNumber: companies.length, teamNumber: teams.length, ticketNumber: tickets.length, leaveNumber: leaves.length});
+    } catch (e: unknown) {
+      console.log(e)
+      res.status(500).send({ error: "Internal server error" });
+    }
+  });
+}
--- src/routes/company/index.ts
+++ src/routes/company/index.ts
@@ -1,1 +1,2 @@
 export * from './getBan'
+export * from './getAllDataAdmin'
