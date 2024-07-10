7bd50af5f46b4320d004c5ff06d7617d60c5811e
DUPLESSIS Alexis <98809630+meizarkos@users.noreply.github.com>
2024-07-10 23:33:36 +0200

    Add of annonce param

--- src/models/crud.ts
+++ src/models/crud.ts
@@ -107,7 +107,8 @@ export const avis: CrudAdmin = {
 export const annonce: CrudAdmin = {
     model: models.Annonce,
     route: '/annonce',
-    forbidden: ['uuid']
+    forbidden: ['uuid'],
+    champNameToFillWithTokenId: 'company_id'
 }
 
 export const offer: CrudAdmin = {
