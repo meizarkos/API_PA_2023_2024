9ba11c059332616a3d9cbc165db08c7b8ef491e7
DUPLESSIS Alexis <98809630+meizarkos@users.noreply.github.com>
2024-07-20 23:10:02 +0200

    Correction

--- src/models/offer.ts
+++ src/models/offer.ts
@@ -12,53 +12,13 @@ export const OfferModel = (sequelize: Sequelize) => {
             type: DataTypes.UUID,
             allowNull: false,
         },
-        situation_id:{
-            type: DataTypes.UUID,
-            allowNull: true,
-        },
         user_id:{
           type: DataTypes.UUID,
           allowNull: false,
         },
-        date_start:{
-            type: DataTypes.DATE,
+        text:{
+            type: DataTypes.TEXT,
             allowNull: false,
-            validate: {
-                notEmpty: { msg: "Date start is required"},
-                notNull: { msg: "Date start is required"},
-            }
-        },
-        date_end:{
-            type: DataTypes.DATE,
-            allowNull: true,
-        },
-        time_start:{
-            type: DataTypes.TIME,
-            allowNull: false,
-            validate: {
-                notEmpty: { msg: "Time start is required"},
-                notNull: { msg: "Time start is required"},
-            }
-        },
-        time_end:{
-            type: DataTypes.TIME,
-            allowNull: false,
-            validate: {
-              notEmpty: { msg: "Time end is required"},
-              notNull: { msg: "Time end is required"},
-            }
-        },
-        frequency:{
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-        price:{
-            type: DataTypes.FLOAT,
-            allowNull: false,
-            validate: {
-                notEmpty: { msg: "Price is required"},
-                notNull: { msg: "Price is required"},
-            }
         },
         status:{
             type: DataTypes.STRING,
